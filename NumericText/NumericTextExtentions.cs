a39b03db874c068f46c30ec5f640eee6bdbeb40b
Eddie Gahan <eddie@portez.com>
2018-11-20 18:03:01 +0000

    Format Documents

--- NumericText/NumericTextExtentions.cs
+++ NumericText/NumericTextExtentions.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace NumericText
 {
-    public static class NumericTextExtentions 
+    public static class NumericTextExtentions
     {
+        private class MatchResult
+        {
+            public string Value { get; set; }
+            public bool IsFraction { get; set; }
+            public bool IsOrdinal { get; set; }
+        }
+
         public static string ToText(this string sSource)
         {
-            string sOutput = "";
+            string sOutput = sSource;
             string sExtractionPattern = "";
-
+            List<MatchResult> MatchResults = new List<MatchResult>();
 
             sSource = "This is a test to find the numbers:1, 101, 2222 or the negative numbers -2345, -44444.55 or -1,234.55 or ordinal numbers like 1st or 22nd or 66th or a number with thousands separators like 1,442,333 in this string. Fractions like 1/10, 3/8, 5/16 should also be located.  This sentence checks for a number at the end of a sentence like 3,244.";
 
@@ -17,26 +26,71 @@ namespace NumericText
 
             // Date, Time, Phone numbers
             // Currencies, percent, to the power of
+            // Heights, Lengths etc.
+            // 
 
             sExtractionPattern = @"(?<fraction>[\-\+]?[0-9]{1,}[\\\/][0-9]{1,})|(?:[\-\+]?[0-9]{4,})(?:(?:\.\d{1,})|(?<ordinal>st|nd|rd|th))?|(?:[\-\+]?[0-9]{1,3}(?:,[0-9]{3})*)(?:(?:\.\d{1,})|(?<ordinal>st|nd|rd|th))?";
 
             MatchCollection oMatches = Regex.Matches(sSource, sExtractionPattern);
 
-            foreach(Match mMatch in oMatches)
+            foreach (Match mMatch in oMatches)
             {
-                Console.WriteLine(mMatch.Value);
+                MatchResults.Add(new MatchResult()
+                {
+                    Value = mMatch.Value,
+                    IsFraction = (mMatch.Groups["fraction"].Value != string.Empty ? true : false),
+                    IsOrdinal = (mMatch.Groups["ordinal"].Value != string.Empty ? true : false)
+                });
             }
 
-            // Remove all non-numeric characters and leave only digits and the first decimal point.
+            // Order the numbers found by their length
 
+            MatchResults = MatchResults.OrderByDescending(x => x.Value.Length).ToList();
 
-            // Check for any ordinal, fraction or currency markers and flag as appropiate. 
+            // Parse the numbers found and replace within the text
 
+            foreach (MatchResult mMatch in MatchResults)
+            {
+                if (mMatch.IsFraction)
+                {
+                }
 
-            // Parse the numbers
+                if (mMatch.IsOrdinal)
+                {
+                }
 
+                if (!mMatch.IsOrdinal && !mMatch.IsFraction)
+                {
+                    sSource = sSource.Replace(mMatch.Value, mMatch.Value.ToNumericText());
+                }
+            }
 
             // Return the parsed string.
+
+            return sOutput;
+        }
+
+        public static string ToNumericText(this string sSource)
+        {
+            return ConvertToText(float.Parse(sSource), false);
+        }
+        public static string ToNumericText(this int iSource)
+        {
+            return ConvertToText((float)iSource, true);
+        }
+        public static string ToNumericText(this float fSource)
+        {
+            return ConvertToText(fSource, false);
+        }
+
+        private static string ConvertToText(float fInput, bool bIgnoreZeroDecimals)
+        {
+            string sOutput = "";
+
+
+            // [minus ] trillion[, ]billion[, ]million[, ]thousand[, ]hundred[ and ]tens/singles[point][digits]
+
+
 
             return sOutput;
         }
